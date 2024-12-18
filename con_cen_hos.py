2c04320143244bd1fe2a161d21ab1c9e598ace04
alex <alexarrieg@gmail.com>
2024-12-18 13:30:24 +0100

    finito scraping

--- con_cen_hos.py
+++ con_cen_hos.py
@@ -1,22 +1,24 @@
 import geopandas as gpd
 
 consultorios = gpd.read_file("input_data/DatosNmc/consultoriosalud.shp")
-output_path = "output_data/consultorios.csv"
-consultorios.to_csv(output_path, index=False)
-print(f"output saved to {output_path}")
+link_consultorios = consultorios[["ETIQUETA", "URL"]]
+link_consultorios.to_csv("output_data//link_consultorios.csv", index=False)
+# output_path = "output_data/consultorios.csv"
+# consultorios.to_csv(output_path, index=False)
+# print(f"output saved to {output_path}")
 
-centros_salud = gpd.read_file("input_data/DatosNmc/centrosalud.shp")
-output_path = "output_data/centros_salud.csv"
-centros_salud.to_csv(output_path, index=False)
-print(f"output saved to {output_path}")
+# centros_salud = gpd.read_file("input_data/DatosNmc/centrosalud.shp")
+# output_path = "output_data/centros_salud.csv"
+# centros_salud.to_csv(output_path, index=False)
+# print(f"output saved to {output_path}")
 
-hospitales = gpd.read_file("input_data/DatosNmc/hospital.shp")
-output_path = "output_data/hospitales.csv"
-hospitales.to_csv(output_path, index=False)
-print(f"output saved to {output_path}")
+# hospitales = gpd.read_file("input_data/DatosNmc/hospital.shp")
+# output_path = "output_data/hospitales.csv"
+# hospitales.to_csv(output_path, index=False)
+# print(f"output saved to {output_path}")
 
-zonasalud = gpd.read_file("input_data/DatosNmc/zonasaludunica.shp")
-zonasalud = zonasalud.drop(columns=["geometry"])
-output_path = "output_data/zonasalud.csv"
-zonasalud.to_csv(output_path, index=False)
-print(f"output saved to {output_path}")
+# zonasalud = gpd.read_file("input_data/DatosNmc/zonasaludunica.shp")
+# zonasalud = zonasalud.drop(columns=["geometry"])
+# output_path = "output_data/zonasalud.csv"
+# zonasalud.to_csv(output_path, index=False)
+# print(f"output saved to {output_path}")
